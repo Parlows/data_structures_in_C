b188baf491c404fc1f9176c2ca5b5591fd9fb808
Pablo Regodon <pablo.regodon@hotmail.com>
2024-09-27 22:32:59 +0200

    added comments

--- trees/linked_tree.h
+++ trees/linked_tree.h
@@ -3,11 +3,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+/* Data model */
 struct data {
     int number;
     int original_position;
 };
 
+/* Node model */
 struct node {
     struct data data;
     struct node * father;
@@ -18,27 +20,60 @@ struct node {
 // Define nodetype
 typedef struct node node_t;
 
-// To build the tree
+/**
+ * Creates the root node and populates it with the number passed as parameter.
+ */
 node_t * make_tree(int number);
 
-// To get the contents of a node
+/**
+ * Returns contents of the node passed as parameter.
+ */
 struct data info(node_t * node);
 
-// To get info about the nature of a node
+/**
+ * Returns true if the node passed as parameter is the left son of his father.
+ */
 bool is_left(node_t * node);
+
+/**
+ * Returns true if the node passed as parameter is the right son of his father.
+ */
 bool is_right(node_t * node);
 
-// To get nodes realted to current node
+/**
+ * Returns the left son of the node passed as parameter.
+ */
 node_t * left(node_t * node);
+
+/**
+ * Returns the right node of the node passed as parameter.
+ */
 node_t * right(node_t * node);
+
+/**
+ * Returns the father node of the node passed as parameter.
+ */
 node_t * father(node_t * node);
+
+/**
+ * Returns the brother of the node passed as parameter.
+ */
 node_t * brother(node_t * node);
 
-// To set contents of node
+/**
+ * Creates a left son of the node with the data passed as parameters.
+ */
 node_t * set_left(node_t * node, int number, int index);
+
+/**
+ * Creates a right son of the node with the data passed as parameters.
+ */
 node_t * set_right(node_t * node, int number, int index);
 
-// To delete the tree and free the memory
+/**
+ * Deletes the tree which spawnes from the node passed as parameter, and the node
+ * itself.
+ */
 void delete(node_t * root);
 
 #endif
