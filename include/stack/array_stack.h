6d10fa17dcea32b93d4fe27181d1a3984c682528
Parlows <pablo.regodon@hotmail.com>
2025-10-12 17:40:03 +0200

    corrected array stack docstrings

--- include/stack/array_stack.h
+++ include/stack/array_stack.h
@@ -16,54 +16,54 @@ struct array_stack {
 
 /**
  * Initializes new stack
- * @param size: maximum number of items the stack new stack will be able to store.
- * @returns: a pointer to the new stack.
+ * @param size maximum number of items the stack new stack will be able to store.
+ * @returns a pointer to the new stack.
  */
 struct array_stack * new_stack(unsigned int size);
 
 /**
  * Frees stack from memory.
- * @param s: pointer to the stack to be freed.
+ * @param s pointer to the stack to be freed.
  */
 void free_stack(struct array_stack * s);
 
 /**
  * Check if the stack is empty.
- * @param s: the stack to insepct.
- * @returns: 1 if the stack is empty, 0 otherwise.
+ * @param s the stack to insepct.
+ * @returns 1 if the stack is empty, 0 otherwise.
  */
 char is_empty(struct array_stack s);
 
 /**
  * Stores the top of the stack in the address pointed by e, and decrements the
  * stack index.
- * @param s: the address of the stack.
- * @param e: address where the popped element will be stored.
- * @returns: 0 if the item was popped, -1 if the stack is empty.
+ * @param s the address of the stack.
+ * @param e address where the popped element will be stored.
+ * @returns 0 if the item was popped, -1 if the stack is empty.
  */
 int pop(struct array_stack * s, unsigned int * e);
 
 /**
  * Stores the item pointed by e in the top of the stack, and increments the
  * stack index.
- * @param s: the address of the stack.
- * @param e: the item to be pushed into the stack.
- * @returns: 0 if the item was pushed, -1 if the stack is full.
+ * @param s the address of the stack.
+ * @param e the item to be pushed into the stack.
+ * @returns 0 if the item was pushed, -1 if the stack is full.
  */
 int push(struct array_stack * s, unsigned int e);
 
 /**
  * Stores the top of the stack in the address pointed by e, but without
  * decrementing the stack index.
- * @param s: the stack.
- * @param e: address where the peeked element will be stored.
- * @returns: 0 if the item was peeked, -1 if the stack is empty.
+ * @param s the stack.
+ * @param e address where the peeked element will be stored.
+ * @returns 0 if the item was peeked, -1 if the stack is empty.
  */
 int peek(struct array_stack s, unsigned int * e);
 
 /**
  * Prints the elements of the stack to stdout.
- * @param s: stack to be printed
+ * @param s stack to be printed
  */
 void print_stack(struct array_stack s);
 
